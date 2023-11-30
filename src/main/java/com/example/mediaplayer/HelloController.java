594409e32a6feaef7d6dee495bcf827d89dde12d
Shailesh kumar <skrshailesh1996@gmail.com>
2023-11-30 19:19:38 +0530

    StackPlane,MediaView,VBox,HBox added

--- src/main/java/com/example/mediaplayer/HelloController.java
+++ src/main/java/com/example/mediaplayer/HelloController.java
@@ -1,14 +1,95 @@
 package com.example.mediaplayer;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
+import javafx.stage.FileChooser;
 
-public class HelloController {
-    @FXML
-    private Label welcomeText;
+import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HelloController implements Initializable {
+
+    private MediaPlayer mediaPlayer;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private MediaView mediaView;
+
+    private String filePath;
+
+    @FXML
+    protected void onHelloButtonClick(ActionEvent event) {
+        FileChooser fileChooser= new FileChooser();
+        FileChooser.ExtensionFilter filter= new FileChooser.ExtensionFilter("Select a File(*.mp4)","*.mp4");
+        fileChooser.getExtensionFilters().add(filter);
+        File file= fileChooser.showOpenDialog(null);
+        filePath= file.toURI().toString();
+
+        if (filePath !=null){
+            Media media = new Media(filePath);
+            mediaPlayer = new MediaPlayer(media);
+            mediaView.setMediaPlayer(mediaPlayer);
+            mediaPlayer.play();
+
+        }
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -10,6 +10,7 @@ module com.example.mediaplayer {
     requires org.kordamp.bootstrapfx.core;
   //  requires eu.hansolo.tilesfx;
     requires com.almasb.fxgl.all;
+    requires javafx.media;
 
     opens com.example.mediaplayer to javafx.fxml;
     exports com.example.mediaplayer;
